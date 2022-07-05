530d99ac79c8ab105f355330c41d7431f5fe9dd3
Nathan Parente <nathan.parente22@gmail.com>
2022-07-05 10:55:24 -0300

    Aula36 Requisião Simples

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,10 +3,33 @@ import { Link } from 'react-router-dom';
 
 export class Home extends Component {
 
+	constructor(props){
+		super(props);
+		this.state = {
+			ip:''
+		}
+	}
+
+	componentDidMount(){
+		fetch('http://ip.jsontest.com/').then((r)=>{
+			return r.json()
+		}).then((json)=>{
+			this.setState({ip:json.ip})
+		})
+	}
+
 	render() {
 		return (
 			<div>
-				Página Home...
+				<h3>Página Home...</h3>
+				<div>
+					{this.state.ip == '' && 
+						<i>Carregando...</i>
+					}
+					{this.state.ip != '' &&
+						<p>Seu IP é: {this.state.ip}</p>
+					}
+				</div>
 
 				<Link to="/sobre">Ir para página sobre</Link>
 			</div>
