cd6f86963106a5bcba673f72116ae52201f620f1
Nathan Parente <nathan.parente22@gmail.com>
2022-07-05 11:19:52 -0300

    Aula38 Requisição Post com dados

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -6,15 +6,13 @@ export class Home extends Component {
 	constructor(props){
 		super(props);
 		this.state = {
-			ip:''
+			lista:[]
 		}
 	}
 
 	componentDidMount(){
-		fetch('http://ip.jsontest.com/').then((r)=>{
-			return r.json()
-		}).then((json)=>{
-			this.setState({ip:json.ip})
+		fetch('https://jsonplaceholder.typicode.com/posts').then(r => r.json()).then(json => {
+			this.setState({lista:json})
 		})
 	}
 
@@ -22,15 +20,20 @@ export class Home extends Component {
 		return (
 			<div>
 				<h3>Página Home...</h3>
-				<div>
-					{this.state.ip == '' && 
-						<i>Carregando...</i>
-					}
-					{this.state.ip != '' &&
-						<p>Seu IP é: {this.state.ip}</p>
-					}
-				</div>
+				{this.state.lista.length == 0 && 
+					<i>Carregando...</i>
+				}
+				{this.state.lista.length > 0 && 
+					<ul>
+						{this.state.lista.map((item)=>{
+							return(
+								<li>#{item.id} - {item.title}</li>
+							)
+						})}
+					</ul>
+				}
 
+				<br/>
 				<Link to="/sobre">Ir para página sobre</Link>
 			</div>
 		);
